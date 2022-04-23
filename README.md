7b9fc341017c5ff8bdac4130d81e7ad68c9cc20c
Руслан Бакиров <ruslanbakirov@MacBook-Air-Ruslan.local>
2022-04-23 11:04:37 +0300

    fix: DATABASE & validations

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # movies-explorer-api
+
+__movies-explorer:__ bakirov.movies.nomoredomains.work
+__api:__ api.bakirov.movies.nomoredomains.work
+__ip:__ 51.250.71.37
--- app.js
+++ app.js
@@ -1,31 +1,41 @@
+const { NODE_ENV, DATABASE } = process.env;
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const { errors } = require('celebrate');
 const user = require('./routes/user');
 const movie = require('./routes/movie');
 const { createUser, login, logout } = require('./controllers/user');
+const { validateCreateUser, validateLogin } = require('./middlewares/validations');
 const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/errorHandler');
+const ErrorNotFound = require('./errors/ErrorNotFound');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
+mongoose.connect(NODE_ENV === 'production' ? DATABASE : 'mongodb://localhost:27017/bitfilmsdb');
 
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/signup', createUser);
-app.post('/signin', login);
+app.post('/signup', validateCreateUser, createUser);
+app.post('/signin', validateLogin, login);
 app.delete('/signout', logout);
 
 app.use(auth);
 
 app.use('/', user);
 app.use('/', movie);
+
+app.use(errors());
+
+app.use((req, res, next) => {
+  next(new ErrorNotFound('Запрашиваемый ресурс не найден'));
+});
 
 app.use(errorHandler);
 
